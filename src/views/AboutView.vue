15913ac316250005ab3bce405d5e37a7b4e2b571
fox.yang <fox.yang@facilityone.cn>
2024-04-19 14:35:48 +0800

    test

---
+++ src/views/AboutView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+import {useI18n} from 'vue-i18n';
+
+export default defineComponent({
+  name: 'AboutView',
+
+  setup() {
+    const {t} = useI18n();
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <fmone-tree :datalist="datalist"></fmone-tree>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from 'vue';
+
+export default defineComponent({
+  name: 'HomeView',
+
+  setup() {
+    const datalist = ref([
+      {
+        label: 'Level one 1',
+        children: [
+          {
+            label: 'Level two 1-1',
+            children: [
+              {
+                label: 'Level three 1-1-1',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'Level one 2',
+        children: [
+          {
+            label: 'Level two 2-1',
+            children: [
+              {
+                label: 'Level three 2-1-1',
+              },
+            ],
+          },
+          {
+            label: 'Level two 2-2',
+            children: [
+              {
+                label: 'Level three 2-2-1',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'Level one 3',
+        children: [
+          {
+            label: 'Level two 3-1',
+            children: [
+              {
+                label: 'Level three 3-1-1',
+              },
+            ],
+          },
+          {
+            label: 'Level two 3-2',
+            children: [
+              {
+                label: 'Level three 3-2-1',
+              },
+            ],
+          },
+        ],
+      },
+    ])
+    return {datalist}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
